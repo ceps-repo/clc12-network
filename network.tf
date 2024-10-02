5181a1dd92be275c16b380e63814ac0ae0f069ca
EC2 Default User <ec2-user@ip-172-31-53-3.ec2.internal>
2024-10-02 01:36:27 +0000

    Add: na minha maquina funciona 3

--- network.tf
+++ network.tf
@@ -64,3 +64,21 @@ resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.subnet2.id
   route_table_id = aws_route_table.rt.id
 }
+
+
+resource "aws_eip" "nat_gw_ip" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "natgw_1a" {
+  allocation_id = aws_eip.nat_gw_ip.id
+  subnet_id     = aws_subnet.subnet1.id
+
+  tags = {
+    Name = "tf_natgw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
